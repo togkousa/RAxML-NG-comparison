1e0a719534433b3dff2b680113001d832a7d9208
Julia <schmid.julia96@gmail.com>
2023-03-20 16:03:31 +0200

    wip: comparison pipeline v1

--- plots/layouts.py
+++ plots/layouts.py
@@ -4,15 +4,15 @@ import dash_bootstrap_components as dbc
 
 def get_empty_dropdown(label, dropdown_id):
     return html.Div(
-                    [
-                        dbc.Label(label),
-                        dcc.Dropdown(
-                            id=dropdown_id,
-                            options=[],
-                            value=None,
-                        ),
-                    ]
-                )
+        [
+            dbc.Label(label),
+            dcc.Dropdown(
+                id=dropdown_id,
+                options=[],
+                value=None,
+            ),
+        ]
+    )
 
 
 layout = html.Div(
@@ -23,23 +23,64 @@ layout = html.Div(
                 html.Br(),
                 html.H1("RAxML-NG version comparison"),
                 html.Br(),
-                dbc.Row([
-                    dbc.Col(get_empty_dropdown(label="Dataset", dropdown_id="datasetSelector"),),
-                    dbc.Col(get_empty_dropdown(label="RAxML-NG Command", dropdown_id="commandSelector")),
-                ]),
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            get_empty_dropdown(
+                                label="Dataset", dropdown_id="datasetSelector"
+                            ),
+                        ),
+                        dbc.Col(
+                            get_empty_dropdown(
+                                label="RAxML-NG Command", dropdown_id="commandSelector"
+                            )
+                        ),
+                    ]
+                ),
                 html.Hr(),
                 html.Br(),
-                html.Div([
-                    html.H5("Comparison entire run"),
-                    dbc.Col(get_empty_dropdown(label="Metric", dropdown_id="resultMetricSelector"),),
-                    dcc.Graph(id="metricEntireRunComparison")
-                ]),
-                html.Br(),
-                html.Div([
-                    html.H5("Comparison all ML trees"),
-                    dbc.Col(get_empty_dropdown(label="Metric", dropdown_id="resultMetricAllTreesSelector"),),
-                    dcc.Graph(id="metricAllTreesComparison")
-                ])
+                html.Div(
+                    [
+                        html.H3("Intra-version comparison"),
+                        html.P("The following plots summarize the results per tested RAxML-NG version separately. "
+                               "That means, that e.g. the RF-Distances between trees always refer to the trees produced"
+                               " by the same tool, and the statistical tests were only ran on the trees per tool."),
+                        html.Br(),
+                        html.Div(
+                            [
+                                html.H5("Comparison entire run"),
+                                dbc.Col(
+                                    get_empty_dropdown(
+                                        label="Metric",
+                                        dropdown_id="resultMetricSelector",
+                                    ),
+                                ),
+                                dcc.Graph(id="metricEntireRunComparison"),
+                            ]
+                        ),
+                        html.Br(),
+                        html.Div(
+                            [
+                                html.H5("Comparison all ML trees"),
+                                dbc.Col(
+                                    get_empty_dropdown(
+                                        label="Metric",
+                                        dropdown_id="resultMetricAllTreesSelector",
+                                    ),
+                                ),
+                                dcc.Graph(id="metricAllTreesComparison"),
+                            ]
+                        ),
+                    ]
+                ),
+                # html.Div(
+                #     [
+                #         html.H3("Inter-version comparison"),
+                #         html.P("The following plots summarize the results when comparing all trees across all RAxML-NG tools. "
+                #                "That means, that e.g. the RF-Distances between trees always refer the RF-Distances ."),
+                #         html.Br(),
+                #     ]
+                # )
             ]
         )
     ]
